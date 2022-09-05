70f986cab4fb74205cc5a0f6a522800d41ec5843
Khemmathat <khemmathat.kong@kmutt.ac.th>
2022-09-05 20:31:24 +0700

    Writed fillstartword function

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,9 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if(String(startWord).match(word)) {
+    console.log(word)
+  }
+  console.log(startWord + word)
 }
 module.exports = fillStartWord
