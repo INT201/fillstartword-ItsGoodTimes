a13a8307696faf49f8a75f8f2627d453e61c9f5f
Khemmathat <khemmathat.kong@kmutt.ac.th>
2022-09-08 13:00:44 +0700

    Completed

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,10 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  if(word.includes(startWord)) {
-    return word
-  } else {
-      return String.prototype.concat(startWord,word)
-  }
+  if(startWord === null || startWord === undefined || word === null || word === undefined) {
+    return undefined
+    }else {
+     if(word.includes(startWord)) {
+        return word
+      } else {
+        return String.prototype.concat(startWord,word)
+      }
+    }  
 }
 module.exports = fillStartWord
